28989388260078241b7dc627ce78aa639ec56fdf
Mohammad ABDUL <abdul.opto@gmail.com>
2025-02-05 16:45:36 +0100

    Route complete et template qui va avec terminés

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,5 +16,28 @@ final class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
+    }
+
+    #[Route('/complete', name: 'app_complete', methods: ['POST'])]
+    public function complete(Request $request, EntityManagerInterface $em): Response
+    {
+        $username = $request->getPayload()->get('username');    
+        $fullname = $request->getPayload()->get('fullname'); 
+        // dd($username);
+
+        if (!empty($username) && !empty($fullname)) {
+            $user = $this->getUser();
+            $user->setUsername($username)
+                ->setFullname($fullname);
+            $em->persist($user);
+            $em->flush();
+            
+            $this->addFlash('success', 'Votre profil est complété');
+        } else {
+
+            $this->addFlash('error', 'Vous devez remplir tous les champs');
+        }
+
+        return $this->redirectToRoute('app_profile');
     }
 }
