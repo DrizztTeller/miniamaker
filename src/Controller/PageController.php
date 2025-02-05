9313957347991ef713f8b00325ae5d344d65285d
Mohammad ABDUL <abdul.opto@gmail.com>
2025-02-05 10:24:04 +0100

    Utilisation du service de loginHistory dans la route index de PageController

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,14 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\LoginHistoryService;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class PageController extends AbstractController{
     #[Route('/', name: 'app_homepage', methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request, LoginHistoryService $lHS): Response
     {
+        // dd($request);
+
+        if ($this->getUser() && $request->headers->get('referer') === 'https://127.0.0.1:8000/login') {
+            $lHS->addHistory($this->getUser(), $request->headers->get('user-agent'), $request->getClientIp());
+        }
+
         if (!$this->getUser()) {
             return $this->render('page/lp.html.twig');
         }
