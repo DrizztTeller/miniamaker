e201cfcc57ecff47d31f659fe0cfff1ec2923d86
Mohammad ABDUL <abdul.opto@gmail.com>
2025-02-05 15:13:13 +0100

    Liaison variable d'environnement à l'appli (services.yaml) et utilisation de celle-ci dans PageController, et amélioration codage et chargement de page selon l'user (co ou non, infos fournies ou non)

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,22 +8,33 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-final class PageController extends AbstractController{
+final class PageController extends AbstractController
+{
     #[Route('/', name: 'app_homepage', methods: ['GET'])]
     public function index(Request $request, LoginHistoryService $lHS): Response
     {
-        // dd($request);
-
-        if ($this->getUser() && $request->headers->get('referer') === 'https://127.0.0.1:8000/login') {
-            $lHS->addHistory($this->getUser(), $request->headers->get('user-agent'), $request->getClientIp());
-        }
-
         if (!$this->getUser()) {
             return $this->render('page/lp.html.twig');
-        }
+        } else {
+            $requestArray = [
+                "fromLogin" => $this->getParameter('APP_URL') . $this->generateUrl('app_login'),
+                "referer" => $request->headers->get('referer'),
+                "user-agent" => $request->headers->get('user-agent'),
+                "ip" => $request->getClientIp(),
 
-        return $this->render('page/homepage.html.twig', [
-            'controller_name' => 'PageController',
-        ]);
+            ];
+
+            if ($requestArray['referer'] === $requestArray['fromLogin']) {
+                $lHS->addHistory($this->getUser(), $requestArray['user-agent'], $requestArray['ip']);
+            }
+
+            if (!$this->getUser()->isComplete()) {
+                return $this->render('page/complete.html.twig');
+            }
+            
+            return $this->render('page/homepage.html.twig', [
+                'controller_name' => 'PageController',
+            ]);
+        }
     }
 }
