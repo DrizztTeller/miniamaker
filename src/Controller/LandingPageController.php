58300a660ef228cb735236237138cfdb38dcaa27
Mohammad ABDUL <abdul.opto@gmail.com>
2025-02-11 11:03:43 +0100

    controller pour landing pages

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[IsGranted('ROLE_USER')]
 final class LandingPageController extends AbstractController{
     #[Route('/landing/add', name: 'lp_add')]
     public function add(): Response
     {
-        $user = $this->getUser();
+        $user = $this->getUser()->getRoles()[0];
 
-        if (in_array('ROLE_AGENT', $user->getRoles()) || in_array('ROLE_PRO', $user->getRoles())) {
+        if ($user !== 'ROLE_PRO' && $user !== 'ROLE_AGENT') {
             return $this->redirectToRoute('app_detail');
         }
 
