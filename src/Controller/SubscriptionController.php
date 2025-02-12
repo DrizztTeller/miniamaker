18f3d37213e3a627f39a53166a6737d7deed25ef
Mohammad ABDUL <abdul.opto@gmail.com>
2025-02-12 14:39:44 +0100

    maj codage pour les paiements

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -17,28 +17,34 @@ final class SubscriptionController extends AbstractController {
     public function subscription(Request $request, PaymentService $ps): RedirectResponse
     {
         try {
-            $subsUser = $this->getUser()->getSubscription();
-            // dd($request->get('plan'));
-    
-            if ($subsUser == null || $subsUser->isActive() === false) {
+            $subscription = $this->getUser()->getSubscription();
+
+            if ($subscription == null || $subscription->isActive() === false) {
                 $checkoutUrl = $ps->setPayment(
                     $this->getUser(),
                     intval($request->get('plan'))
                 );
-    
-                // dd($checkoutUrl);
-                $this->redirect($checkoutUrl);
-                return new RedirectResponse(url: $checkoutUrl);
-            } else {
-                $this->addFlash('warning', 'Vous avez déjà abonné(e)');
-                return $this->redirectToRoute('app_profile');
+                return $this->redirectToRoute('app_subscription_check', ['link' => $checkoutUrl]);
+                // return new RedirectResponse($checkoutUrl);
             }
+
+            $this->addFlash('warning', "Vous êtes déjà abonné(e)");
+            return $this->redirectToRoute('app_profile');
         } catch (\Exception $e) {
             $this->addFlash('error', 'Une erreur est survenue lors de la création du paiement');
             return $this->redirectToRoute('app_profile');
         }
-       
     }
+
+    #[Route('/subscription/check', name: 'app_subscription_check')]
+    public function check(Request $request): Response
+    {
+        // Logique de traitement du succès
+        return $this->render('subscription/check.html.twig', [
+            'link' => $request->get('link'),
+        ]);
+    }
+    
     #[Route('/subscription/success', name: 'app_subscription_success', methods: ['GET'])]
     public function subsSucces(): Response
     {
